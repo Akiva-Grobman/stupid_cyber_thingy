04c102c7cff6022c2c0ecfe3adfc4bef3db0ef2c
akiva <akiva.grobman@gmail.com>
2022-01-22 21:09:42 +0200

    Merge new implementation

--- main.py
+++ main.py
@@ -1,52 +1,75 @@
+import copy
 import curses
 import random
 from collections import namedtuple
+from dataclasses import dataclass
+from typing import List
+
 import pyfiglet
 
-
 vector = namedtuple('Vector', 'x y')
-counter = 0
-display_background = []
+
+
+@dataclass
+class IndexData:
+    position_in_line: int = 0
+    color: int = 1
+    ascii_representation: str = ' '
+
+
+background: List[List[IndexData]] = []
+# line
+updating_info: List[IndexData] = []
 
 
 def cyber_rain(window):
     display_text = get_display_text()
     curses.curs_set(curses.A_INVIS)
+    init_colors()
     init_background()
     while True:
-        update_background(window)
+        curses.update_lines_cols()
+        update_background()
+        window.clear()
+        draw_background(window)
         draw_text(display_text, window)
         window.refresh()
-        # you might want to use sleep to slow down the refresh rate
-        # time.sleep(0.002)
+
+
+def init_data() -> IndexData:
+    if random.randint(0, 15) != 1:
+        return IndexData()
+    line_length = random.randint(5, 15)
+    return IndexData(
+        position_in_line=line_length,
+        color=curses.color_pair(random.randint(1, 5)),  # add random
+        ascii_representation=str(random.randint(0, 1))
+    )
+
+
+def init_display_info():
+    for _ in range(curses.COLS - 1):
+        updating_info.append(init_data())
 
 
 def init_background():
-    global display_background
-    display_background.clear()
-    for _ in range((curses.LINES - 1) * 2):
-        random_line = ''.join(
-            [' ' * random.randint(0, 2)
-             + str(random.randint(0, 1))
-             + ' ' * random.randint(0, 3)
-             for _ in range(curses.COLS - 1)]
-        )[:curses.COLS - 1]
-        display_background.append(random_line)
+    init_display_info()
+    global background
+    background = [[] for _ in range(curses.LINES - 1)]
+    for line_index in range(curses.LINES - 1):
+        for data_index, data in enumerate(updating_info):
+            background[line_index].append(copy.deepcopy(data))
+            if data.position_in_line != 0:
+                updating_info[data_index].position_in_line -= 1
+            else:
+                updating_info[data_index] = init_data()
 
 
-def update_background(window):
+def init_colors():
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    background_color = curses.color_pair(1)
-    global counter
-    counter += 1
-    background = ''
-    if counter >= curses.LINES - 1:
-        counter = 0
-        init_background()
-    for index in range(counter, counter + curses.LINES - 1):
-        background = f'{background}\n{display_background[index]}'
-    window.clear()
-    window.addstr(0, 0, background, background_color)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
 
 
 def draw_text(display_text, window):
@@ -74,6 +97,37 @@ def get_display_text():
     :return str as ascii text:
     """
     return pyfiglet.figlet_format(list('Cyber'), font='banner3').split('\n')
+
+
+def update_display_info():
+    global updating_info
+
+    # update size
+    if len(updating_info) <= curses.COLS:
+        updating_info = updating_info[:curses.COLS - 1]
+    else:
+        while len(updating_info) > curses.COLS - 1:
+            updating_info.append(init_data())
+
+    for index, data in enumerate(updating_info):
+        if data.position_in_line == 0:
+            updating_info[index] = init_data()
+        else:
+            updating_info[index].position_in_line -= 1
+
+
+def update_background():
+    update_display_info()
+    # remove top line and add new one
+    background.pop(0)
+    new_line = [copy.deepcopy(data) for data in updating_info]
+    background.append(new_line)
+
+
+def draw_background(window):
+    for line_index, line in enumerate(reversed(background)):
+        for column_index, position_data in enumerate(line):
+            window.addstr(line_index, column_index, position_data.ascii_representation, position_data.color)
 
 
 def main():
